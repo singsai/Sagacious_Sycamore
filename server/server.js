16bbb59131fa55d32658745128d4525ec15328aa
Haris Muhammad <harismh@icloud.com>
2016-12-07 15:58:46 -0800

    adding server routes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json()); 
 
 var app = express();
 
-app.listen(3000);
+app.get('/', function(req, res) {
+  res.sendFile('./public/index.html');
+});
 
+/*
+app.get('APIROUTE', function(req, res) {
+  DATABASE QUERY
+});
+
+app.post('APIROUTE', function(req, res) {
+  DATABASE INSERT
+})
+*/
+
+app.listen(3000);
 console.log('Server listening on 3000...');
