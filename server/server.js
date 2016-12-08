279e9a5fb9e0895a6fdae23b08248025ab812d5c
Lisa Nam <lsyn2016@gmail.com>
2016-12-07 22:02:14 -0800

    Added authentication

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var db = require('../data/database.js').db;
 var controller = require('./controller.js');
 
@@ -24,15 +26,31 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json()); 
 
+//use cookieparser & session
+app.use(cookieParser('cookies!!!'));
+app.use(session({
+  secret: 'What\'s your secret?',
+  resave: false,
+  saveUninitialized: true
+}));
+
 
 //default index route
 app.get('/', function(req, res) {
-  res.sendFile('./public/index.html');
+  res.sendFile('../public/index.html');
 });
 
 //use controller.js as middleware
 app.get('/api/pet', controller.get); //todo add method
 app.post('/api/pet', controller.post); //todo add method
+
+//route to controller.js
+app.get('/login', function(req, res) {
+  res.redirect('/api/pet');
+})
+app.post('/login', controller.login);
+app.get('/logout', controller.logout);
+app.post('/signup', controller.signup);
 
 app.listen(3000);
 console.log('Server listening on 3000...');
