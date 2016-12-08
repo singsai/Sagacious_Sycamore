bf9a046deb963185bdcb6ba3920d5f6460dc15ae
Haris Muhammad <harismh@icloud.com>
2016-12-07 16:05:36 -0800

    merge conflicts

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var db = require('../data/database.js').db;
 
 //start sequelize database
@@ -15,8 +16,25 @@ db.authenticate()
   });
 
 
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json()); 
+
 var app = express();
 
-app.listen(3000);
+app.get('/', function(req, res) {
+  res.sendFile('./public/index.html');
+});
 
+/*
+app.get('APIROUTE', function(req, res) {
+  DATABASE QUERY
+});
+
+app.post('APIROUTE', function(req, res) {
+  DATABASE INSERT
+})
+*/
+
+app.listen(3000);
 console.log('Server listening on 3000...');
