1d1a24c0eb143390485961548ba9785a40d09929
Emmeline <Emmeline.Lan@gmail.com>
2016-12-08 15:05:01 -0800

    Connect server and client - attempt 1

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ db.authenticate()
 var app = express();
 
 //use bodyparser middleware
-app.use(express.static(__dirname + '../public'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json()); 
 
