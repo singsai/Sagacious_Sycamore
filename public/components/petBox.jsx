76f4eb4b79f2145d6f2b755d9bbdf8a32d352392
Emmeline <Emmeline.Lan@gmail.com>
2016-12-10 15:26:57 -0800

    Fix status bar and delete icons in petBox component

--- public/components/petBox.jsx
+++ public/components/petBox.jsx
@@ -1,47 +1,15 @@
 // image generator with different icons based on pet mood
-var imgSrc = {
-  normal: "http://i.imgur.com/RzBy3Vw.gif",
-  sleep: "http://i.imgur.com/PujjsmB.gif",
-  code: "http://i.imgur.com/KTNujjY.gif",
-  happy: "http://i.imgur.com/jjUbQ6P.gif",
-  feed: "http://i.imgur.com/W8UQN1M.gif",
-  runaway: "http://i.imgur.com/JfH45R0.jpg",
-  dead: "http://i.imgur.com/3tWT7qP.jpg",
-  sick: "http://i.imgur.com/CdIG2m2.gif",
-  play: "http://i.imgur.com/T99KqDs.gif"
-}
-
-
-var Image = function(props) {
-  var icon = props.icon;
-  var levelProgress = props.level * 10 + '%';
-
-  var o = {
-    happy: function() {
-      return <img className='icon' src="http://emojione.com/wp-content/uploads/assets/emojis/1f606.svg"></img>
-    },
-    sad: function() {
-      return <img className='icon' src='http://emojione.com/wp-content/uploads/assets/emojis/1f62d.svg'></img>
-    }
-  }
-
-  return o[icon]? o[icon]() : <span className='glyphicon glyphicon-thumbs-up'></span>;
-}
-
-
 var Petbox = (props) => {
   // progress bar that represents level
   var progressStyle = {
     width: props.pet.level/3 * 100 + '%'
   }
 
-  var currentImage = imgSrc[props.pet.status]
-
   return (
   <div className='petView container'>
     <div className='row'>
       <div className='pet-image-container col-md-6 col-xs-6'>
-        <img className="pet-image" src={currentImage}></img>
+        <img className="pet-image" src={props.pet.img}></img>
       </div>
       <div className='stats col-md-6 col-xs-6'>
         <div className='stats container'>
