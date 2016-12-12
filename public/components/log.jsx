fb84e7175b4c607626697c40581c4ff096fb3260
Lisa Nam <lsyn2016@gmail.com>
2016-12-12 11:33:00 -0800

    fix bugs in logs

--- public/components/log.jsx
+++ public/components/log.jsx
@@ -1,5 +1,5 @@
 var Log = (props) => (
   <div>{
-    props.logs.reverse().slice(props.logs.length - 15).map(function(log, index){return (<span key={index}>{log.name +' is ' + log.action + ' at ' + log.createdAt}<br></br></span> )})
+    props.logs.map(function(log, index){return (<span key={index}>{log.name +' is ' + log.action + ' at ' + log.createdAt}<br></br></span> )})
   }</div>
 )
