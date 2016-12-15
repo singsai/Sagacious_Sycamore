91da7da324c36a967122f107fd17743469a7b93c
Michael Ilg <michael@ilg.ac>
2016-12-14 17:13:14 -0800

    Add Question adding form

--- public/components/addQuestion.jsx
+++ public/components/addQuestion.jsx
@@ -1,28 +1,19 @@
 var Modal = ReactBootstrap.Modal;
 var Button = ReactBootstrap.Button;
+var FormGroup = ReactBootstrap.FormGroup;
+var ControlLabel = ReactBootstrap.ControlLabel;
+var FormControl = ReactBootstrap.FormControl;
+var HelpBlock = ReactBootstrap.HelpBlock;
+var FieldGroup = ReactBootstrap.FieldGroup;
 
 class AddQuestionModal extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: ''
-    }
   }
 
-  getValidationState() {
-    const length = this.state.value.length;
-    if (length > 10) return 'success';
-    else if (length > 5) return 'warning';
-    else if (length > 0) return 'error';
-  }
-
-  handleChange(e) {
-    this.setState({ value: e.target.value });
-  }
-
-//localhost:3000/api/question
-  componentDidMount() {
-    this.props.getQuestion();
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('Question submitted', event);
   }
 
   render() {
@@ -36,27 +27,34 @@ class AddQuestionModal extends React.Component {
         </div>
         <div>
           <Modal show={this.props.showModal} onHide={this.props.toggleModalClick}>
-            <Modal.Header closeButton>
-              <Modal.Title>Coding practice</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <h4>React Coding Challenge</h4>
-              <p>{this.props.question.question}</p>
 
-              <h4>Answer Choices:</h4>
-              <Button bsStyle="primary" value="1" onClick={this.props.pickAnswer}>{this.props.question.choice1}</Button>
-              <p>Some more text</p>
-              <Button bsStyle="primary" value="2" onClick={this.props.pickAnswer}>{this.props.question.choice2}</Button>
-              <p>Some more text</p>
-              <Button bsStyle="primary" value="3" onClick={this.props.pickAnswer}>{this.props.question.choice3}</Button>
-              <p>Some more text</p>
-              <Button bsStyle="primary" value="4" onClick={this.props.pickAnswer}>{this.props.question.choice4}</Button>
-              <p>Some more text</p>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={this.props.submitAnswer}>Submit</Button>
+              <form onSubmit={this.handleSubmit.bind(this)}>
+                <FormGroup controlId="formControlsQuestion">
+                  <ControlLabel>Question</ControlLabel>
+                  <FormControl componentClass="textarea" placeholder="Question" />
+                </FormGroup>
+                <FormGroup controlId="formControlsAnswer1">
+                  <ControlLabel>Answer 1</ControlLabel>
+                  <FormControl componentClass="textarea" placeholder="Answer 1" />
+                </FormGroup>
+                <FormGroup controlId="formControlsAnswer2">
+                  <ControlLabel>Answer 2</ControlLabel>
+                  <FormControl componentClass="textarea" placeholder="Answer 2" />
+                </FormGroup>
+                <FormGroup controlId="formControlsAnswer3">
+                  <ControlLabel>Answer 3</ControlLabel>
+                  <FormControl componentClass="textarea" placeholder="Answer 3" />
+                </FormGroup>
+                <FormGroup controlId="formControlsAnswer4">
+                  <ControlLabel>Answer 4</ControlLabel>
+                  <FormControl componentClass="textarea" placeholder="Answer 4" />
+                </FormGroup>
 
-            </Modal.Footer>
+                <Button type="submit">
+                  Submit
+                </Button>
+              </form>
+
           </Modal>
         </div>
       </div>
