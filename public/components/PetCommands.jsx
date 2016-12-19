b8e90a76f3645fb3a6097a7244a4e5208b19f172
Jarob Gilliam <jarobgilliambiz@gmail.com>
2016-12-18 17:50:13 -0800

    Add game icon

--- public/components/PetCommands.jsx
+++ public/components/PetCommands.jsx
@@ -1,12 +1,12 @@
-var PetCommand = (props) => (
-  <div className="row">
-    <div className="center">
-      <img className='img-responsive col-xs-2' src={props.cmdImg.food} onClick={() => {props.executeCommand('eating')}}/>
-      <img className='img-responsive col-xs-2' src={props.cmdImg.love} onClick={() => {props.executeCommand('playing')}}/>
-      <img className='img-responsive col-xs-2' src={props.cmdImg.code} onClick={() => {props.executeCommand('codingButton')}}/>
-      <img className='img-responsive col-xs-2' src={props.cmdImg.sleep} onClick={() => {props.executeCommand('sleeping')}}/>
-      <img className='img-responsive col-xs-2' src={props.cmdImg.question} onClick={() => {props.executeCommand('question')}}/>
-    </div>
+avar PetCommand = (props) => (
+  <div className="pet-commandbar">
+    <img className='command' src={props.cmdImg.food} onClick={() => {props.executeCommand('eating')}}/>
+    <img className='command' src={props.cmdImg.love} onClick={() => {props.executeCommand('playing')}}/>
+    <img className='command' src={props.cmdImg.code} onClick={() => {props.executeCommand('coding')}}/>
+    <img className='command' src={props.cmdImg.sleep} onClick={() => {props.executeCommand('sleeping')}}/>
+    <img className='command' src={props.cmdImg.question} onClick={() => {props.executeCommand('question')}}/>
+    <a href='/game'><img className='command' src='../other/img/gude.png' /></a>
+
   </div>
 );
 
