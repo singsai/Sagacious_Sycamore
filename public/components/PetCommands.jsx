4b831cdf16371d342202a7903a36a076535aa45f
Nick <Nick@Nicks-MacBook-Pro.local>
2016-12-18 18:17:37 -0800

    Fix quiz icon

--- public/components/PetCommands.jsx
+++ public/components/PetCommands.jsx
@@ -2,7 +2,7 @@ avar PetCommand = (props) => (
   <div className="pet-commandbar">
     <img className='command' src={props.cmdImg.food} onClick={() => {props.executeCommand('eating')}}/>
     <img className='command' src={props.cmdImg.love} onClick={() => {props.executeCommand('playing')}}/>
-    <img className='command' src={props.cmdImg.code} onClick={() => {props.executeCommand('coding')}}/>
+    <img className='command' src={props.cmdImg.code} onClick={() => {props.executeCommand('codingButton')}}/>
     <img className='command' src={props.cmdImg.sleep} onClick={() => {props.executeCommand('sleeping')}}/>
     <img className='command' src={props.cmdImg.question} onClick={() => {props.executeCommand('question')}}/>
     <a href='/game'><img className='command' src='../other/img/gude.png' /></a>
